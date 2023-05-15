ef25c1ce76fd234999c8a510763ea86f2b167633
Abdurrahman Ebrar Koçak <78161216+AEKHer@users.noreply.github.com>
2023-05-16 00:11:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var sonrakibolum1=document.getElementById("next-chapter");
+	sonrakibolum1.onclick=function(){
+		window.location.href = "chapter2.html";
+	}
+
+	var oncekibolum1=document.getElementById("prev-chapter");
+	oncekibolum1.onclick=function(){
+		window.location.href = "chapter1.html";
+	}
